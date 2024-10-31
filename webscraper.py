f92dc75a5f3f64dc92f204bc6ea486f4d2e96203
Elia Zanini <elizan01@libero.it>
2024-10-31 12:08:41 +0100

    gestione dati
    semplice gestione dei dati importati.
    fatto un controllo sul csv, verranno uplodati (quando necessario) sul csv stesso

--- webscraper.py
+++ webscraper.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By  # Importa By
 from time import sleep
 import pyperclip
+import pandas as pd
 
 #percorso al chromedriver
 driver_path = "C:/Users/eliza/Documents/GitHub/covid-19-data-tracker/chromedriver.exe"
@@ -27,7 +28,7 @@ sleep(2)
 driver.find_element(By.CSS_SELECTOR, 'button[aria-label="Copy raw content"]').click()
 sleep(1)
 dati_copiati_italia = pyperclip.paste()
-print(dati_copiati_italia)
+
 
 #importo i dati settimanali sulle regioni
 driver.back()
@@ -39,11 +40,26 @@ sleep(1)
 driver.find_element(By.CSS_SELECTOR, 'button[aria-label="Copy raw content"]').click()
 sleep(1)
 dati_copiati_regioni = pyperclip.paste()
-print(dati_copiati_regioni)
 
-#capire come importarli su dataframe e dove salvarlo
 
-#capire come confrontarli con dati settimana prima
+lista_dati = dati_copiati_italia.split(",")
+new_datastr= lista_dati[23]
+new_data= new_datastr[32:42]
+totale_positivi = int(lista_dati[29]) 
+terapie_intensive = int(lista_dati[26])
+morti_totali= int(lista_dati[33])
+tot_ospedalizzati= int(lista_dati[27])
+tot_casi= int(lista_dati[36])
 
-#capire come fare controllo su dataframe, se dati sono uguali non modificare
+df=pd.read_csv("C:/Users/eliza/Documents/GitHub/covid-19-data-tracker/csv usati/weeklyupdate.csv")
+ultima_data_salvata = df.iloc[0, 1]
+print(ultima_data_salvata)
 
+if ultima_data_salvata == new_data:
+    print('nothing to change')
+#else:
+    dati_settimana = {"data":  [new_data],"totali positivi": [totale_positivi], "casi totali": [tot_casi],  "terapie intensive": [terapie_intensive],"morti totali": [morti_totali],"tot ospedalizzati": [tot_ospedalizzati]}
+    df_nuova_riga = pd.DataFrame(dati_settimana)
+    df = pd.concat([ df_nuova_riga, df])
+    df.to_csv("C:/Users/eliza/Documents/GitHub/covid-19-data-tracker/csv usati/weeklyupdate.csv", index=False)  
+    print('Dati nuova sett. aggiunti')
